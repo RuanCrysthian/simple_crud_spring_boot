3e854f15b2b2e271dbc1fff417412a380eee40f6
RuanCrysthian <ruanlima155@gmail.com>
2023-08-04 20:36:52 -0300

    feat: create BaseModel

---
+++ crud/src/main/java/com/example/crud/model/BaseModel.java
@@ -0,0 +1,52 @@
+package com.example.crud.model;
+
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public abstract class BaseModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @JsonProperty("created_at")
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, length = 80)
+  private LocalDateTime createdAt;
+
+  @JsonProperty("updated_at")
+  @UpdateTimestamp
+  @Column(name = "updated_at", nullable = false, length = 80)
+  private LocalDateTime updatedAt;
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return this.createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return this.updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+}
