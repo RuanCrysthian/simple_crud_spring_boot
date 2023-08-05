84dd03a0fa6bae73083f18088a7e74b0f0a81aed
RuanCrysthian <ruanlima155@gmail.com>
2023-08-04 21:01:28 -0300

    refactor: change name and cpf size

--- crud/src/main/resources/db/migration/V1__Table_Person.sql
+++ crud/src/main/resources/db/migration/V1__Table_Person.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS `crud-springboot` ;
 CREATE TABLE IF NOT EXISTS `person` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(80) NOT NULL,
-  `cpf` varchar(80) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
   `created_at` datetime(6) NOT NULL,
   `updated_at` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
