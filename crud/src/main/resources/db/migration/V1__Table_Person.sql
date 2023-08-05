efda67ba852f9824a050ff452aa3058e68d4115f
RuanCrysthian <ruanlima155@gmail.com>
2023-08-04 22:37:07 -0300

    fix: fix bug in migration

--- crud/src/main/resources/db/migration/V1__Table_Person.sql
+++ crud/src/main/resources/db/migration/V1__Table_Person.sql
@@ -1,9 +1,7 @@
-CREATE DATABASE IF NOT EXISTS `crud-springboot` ;
-CREATE TABLE IF NOT EXISTS `person` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `cpf` varchar(14) NOT NULL,
-  `created_at` datetime(6) NOT NULL,
-  `updated_at` datetime(6) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS person (
+  id SERIAL PRIMARY KEY,
+  name varchar(100) NOT NULL,
+  cpf varchar(14) NOT NULL,
+  created_at timestamp(6) NOT NULL,
+  updated_at timestamp(6) NOT NULL
 );
